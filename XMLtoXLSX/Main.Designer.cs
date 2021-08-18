4f9f7c499accd0856bf94f128ed4c7358de4720c
Lancelot <76182416+ssLancelot@users.noreply.github.com>
2021-08-18 07:09:25 +0300

    Initial commit

---
+++ XMLtoXLSX/Main.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace XMLtoXLSX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtXmlPath = new DevExpress.XtraEditors.TextEdit();
+            this.txtFileName = new DevExpress.XtraEditors.TextEdit();
+            this.chbExcelFileName = new DevExpress.XtraEditors.CheckEdit();
+            this.OFD = new DevExpress.XtraEditors.XtraOpenFileDialog(this.components);
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.btnBrowse = new DevExpress.XtraEditors.SimpleButton();
+            this.btnConvert = new DevExpress.XtraEditors.SimpleButton();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.hyperlinkLabelControl1 = new DevExpress.XtraEditors.HyperlinkLabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.txtXmlPath.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFileName.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chbExcelFileName.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtXmlPath
+            // 
+            resources.ApplyResources(this.txtXmlPath, "txtXmlPath");
+            this.txtXmlPath.Name = "txtXmlPath";
+            // 
+            // txtFileName
+            // 
+            resources.ApplyResources(this.txtFileName, "txtFileName");
+            this.txtFileName.Name = "txtFileName";
+            // 
+            // chbExcelFileName
+            // 
+            resources.ApplyResources(this.chbExcelFileName, "chbExcelFileName");
+            this.chbExcelFileName.Name = "chbExcelFileName";
+            this.chbExcelFileName.Properties.Caption = resources.GetString("chbExcelFileName.Properties.Caption");
+            this.chbExcelFileName.CheckedChanged += new System.EventHandler(this.chbExcelFileName_CheckedChanged);
+            // 
+            // labelControl1
+            // 
+            resources.ApplyResources(this.labelControl1, "labelControl1");
+            this.labelControl1.Name = "labelControl1";
+            // 
+            // btnBrowse
+            // 
+            resources.ApplyResources(this.btnBrowse, "btnBrowse");
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnConvert
+            // 
+            resources.ApplyResources(this.btnConvert, "btnConvert");
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            // 
+            // hyperlinkLabelControl1
+            // 
+            resources.ApplyResources(this.hyperlinkLabelControl1, "hyperlinkLabelControl1");
+            this.hyperlinkLabelControl1.Name = "hyperlinkLabelControl1";
+            this.hyperlinkLabelControl1.Click += new System.EventHandler(this.hyperlinkLabelControl1_Click);
+            // 
+            // Form1
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.Appearance.Options.UseBackColor = true;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.hyperlinkLabelControl1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.chbExcelFileName);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.txtXmlPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.IconOptions.LargeImage = global::XMLtoXLSX.Properties.Resources.convert_32x32;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtXmlPath.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFileName.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chbExcelFileName.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private DevExpress.XtraEditors.TextEdit txtXmlPath;
+        private DevExpress.XtraEditors.TextEdit txtFileName;
+        private DevExpress.XtraEditors.CheckEdit chbExcelFileName;
+        private DevExpress.XtraEditors.XtraOpenFileDialog OFD;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.SimpleButton btnBrowse;
+        private DevExpress.XtraEditors.SimpleButton btnConvert;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private DevExpress.XtraEditors.HyperlinkLabelControl hyperlinkLabelControl1;
+    }
+}
+
---
+++ XMLtoXLSX/Main.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace XMLtoXLSX
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult drResult = OFD.ShowDialog();
+            if (drResult == System.Windows.Forms.DialogResult.OK)
+                txtXmlPath.Text = OFD.FileName;
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            // Resetting the progress bar Value
+            progressBar1.Value = 0;
+
+            if (chbExcelFileName.Checked && txtFileName.Text != "" &&
+            txtXmlPath.Text != "") // using Custom Xml File Name
+            {
+                if (File.Exists(txtXmlPath.Text)) // Checking XMl File is Exist or Not
+                {
+                    string CustXmlFilePath =
+                           Path.Combine(new FileInfo(txtXmlPath.Text).DirectoryName,
+                    txtFileName.Text); // Creating Path for Xml Files
+                    System.Data.DataTable dt = CreateDataTableFromXml(txtXmlPath.Text);
+                    ExportDataTableToExcel(dt, CustXmlFilePath);
+
+                    MessageBox.Show("Conversion Completed!!");
+                }
+
+            }
+            else if (!chbExcelFileName.Checked ||
+                         txtXmlPath.Text != "") // Using Default Xml File Name
+            {
+                if (File.Exists(txtXmlPath.Text)) // Checking XMl File is Exist or Not
+                {
+                    FileInfo fi = new FileInfo(txtXmlPath.Text);
+                    string XlFile = fi.DirectoryName + "\\" + fi.Name.Replace
+                    (fi.Extension, ".xlsx"); // CReating Default File Name
+                    System.Data.DataTable dt = CreateDataTableFromXml
+                    (txtXmlPath.Text); // Getting XML Data into DataTable
+                    ExportDataTableToExcel(dt, XlFile);
+
+                    MessageBox.Show("Dönüştürme Başarılı!!");
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen Bir XML Dosyası Seçiniz");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Gerekli Parametreleri Doğru Giriniz!!");
+            }
+        }
+
+        private void ExportDataTableToExcel(System.Data.DataTable table, string Xlfile)
+        {
+            Microsoft.Office.Interop.Excel.Application excel =
+                                 new Microsoft.Office.Interop.Excel.Application();
+            Workbook book = excel.Application.Workbooks.Add(Type.Missing);
+            excel.Visible = false;
+            excel.DisplayAlerts = false;
+            Worksheet excelWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)book.ActiveSheet;
+            excelWorkSheet.Name = table.TableName;
+
+            progressBar1.Maximum = table.Columns.Count;
+            for (int i = 1; i < table.Columns.Count + 1; i++) // Creating Header Column In Excel
+            {
+                excelWorkSheet.Cells[1, i] = table.Columns[i - 1].ColumnName;
+                if (progressBar1.Value < progressBar1.Maximum)
+                {
+                    progressBar1.Value++;
+                    int percent = (int)(((double)progressBar1.Value /
+                    (double)progressBar1.Maximum) * 100);
+                    progressBar1.CreateGraphics().DrawString(percent.ToString() +
+                    "%", new System.Drawing.Font("Arial",
+                    (float)8.25, FontStyle.Regular), Brushes.Black,
+                    new PointF(progressBar1.Width / 2 - 10, progressBar1.Height / 2 - 7));
+                    System.Windows.Forms.Application.DoEvents();
+                }
+            }
+
+            progressBar1.Maximum = table.Rows.Count;
+            for (int j = 0; j < table.Rows.Count; j++) // Exporting Rows in Excel
+            {
+                for (int k = 0; k < table.Columns.Count; k++)
+                {
+                    excelWorkSheet.Cells[j + 2, k + 1] = table.Rows[j].ItemArray[k].ToString();
+                }
+
+                if (progressBar1.Value < progressBar1.Maximum)
+                {
+                    progressBar1.Value++;
+                    int percent = (int)(((double)progressBar1.Value /
+                                         (double)progressBar1.Maximum) * 100);
+                    progressBar1.CreateGraphics().DrawString(percent.ToString() +
+                    "%", new System.Drawing.Font("Arial",
+                    (float)8.25, FontStyle.Regular), Brushes.Black,
+                    new PointF(progressBar1.Width / 2 - 10, progressBar1.Height / 2 - 7));
+                    System.Windows.Forms.Application.DoEvents();
+                }
+            }
+
+            book.SaveAs(Xlfile);
+            book.Close(true);
+            excel.Quit();
+
+            Marshal.ReleaseComObject(book);
+            Marshal.ReleaseComObject(book);
+            Marshal.ReleaseComObject(excel);
+        }
+        public System.Data.DataTable CreateDataTableFromXml(string XmlFile)
+        {
+            System.Data.DataTable Dt = new System.Data.DataTable();
+            try
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml(XmlFile);
+                Dt.Load(ds.CreateDataReader());
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OFD.Filter = "Xml Dosyaları (.xml)|*.xml";
+        }
+
+        private void chbExcelFileName_CheckedChanged(object sender, EventArgs e)
+        {
+            switch (chbExcelFileName.Checked)
+            {
+                case true:
+                    txtFileName.Enabled = true;
+                    break;
+                case false:
+                    txtFileName.Enabled = false;
+                    break;
+            }
+        }
+
+        private void hyperlinkLabelControl1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://cyberyazilim.com");
+        }
+    }
+}
